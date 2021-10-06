f25935b0bca1de755958682a7b35e372be1f434a
David Verhulst <david.verhulst@ap.be>
2021-10-06 21:54:08 +0200

    preloading images done

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -8,3 +8,13 @@ httpClient.interceptors.request.use(function (config) {
   config.headers.Authorization = `Client-ID ${process.env.UNSPLASH_CLIENTID}`;
   return config;
 });
+
+export const preloadImage = (url) =>
+  new Promise((resolve, reject) => {
+    const img = new Image();
+    img.src = url;
+    img.onload = resolve;
+    img.onerror = reject;
+  });
+
+export const preloadImages = (arr) => Promise.all(arr.map(preloadImage));
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import "../css/style.scss";
 
-import { httpClient } from "./helpers";
+import { httpClient, preloadImage, preloadImages } from "./helpers";
 
 /**
  * VARS
@@ -10,26 +10,38 @@ const searchField = document.getElementById("searchbar");
 const form = document.querySelector("form");
 const cardTemplate = document.querySelector("template").innerHTML;
 const grid = document.getElementById("cards");
-
+const inputZone = document.querySelector("#wrapperforicon");
 /**
  * EVENTS
  */
 
-form.onsubmit = function (e) {
+form.onsubmit = async function (e) {
   e.preventDefault();
   const { value } = searchField;
   if (value.length > 3) {
-    httpClient(`/search/photos?query=${value}`).then((response) => {
-      grid.innerHTML = response.data.results
-        .map((unsplashObj) =>
-          cardTemplate
-            .replace("#CARD_IMAGE_URL", unsplashObj.urls.thumb)
-            .replace("#CARD_PROFILE", "")
-            .replaceAll("#CARD_TEXT", unsplashObj.alt_description)
-        )
-        .join("");
-    });
+    // fetch uitvoeren
+    const response = await httpClient(`/search/photos?query=${value}`);
+    inputZone.classList.add("loading");
+    form.disabled = true;
+    await preloadImages(
+      response.data.results.map((unsplashObj) => unsplashObj.urls.thumb)
+    );
+    inputZone.classList.remove("loading");
+    form.disabled = false;
+    //loader weghalen
+    //grid opbouwen
+    grid.innerHTML = response.data.results
+      .map((unsplashObj) =>
+        cardTemplate
+          .replace("#CARD_IMAGE_URL", unsplashObj.urls.thumb)
+          .replace("#CARD_PROFILE", "")
+          .replaceAll("#CARD_TEXT", unsplashObj.alt_description)
+      )
+      .join("");
   }
+
+  // hoeveel fotos zijn er?
+  // fetch results in grid plaatsen
 };
 
 //alt_description
